b18ccf377b1520757cc1204cbba97ca43d778a2a
wwblodge1 <wwblodge@ischool.berkeley.edu>
2021-09-23 18:29:00 -0700

    face_video_v1

--- face_video.py
+++ face_video.py
@@ -4,7 +4,38 @@ import time
 import paho.mqtt.client as paho
 import sys
 
+#networking stuff
+
+# video feed capture 
+cap = cv.VideoCapture(1)
+
+# Cascade face classifier
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# start stream 
+client.loop_start()
+
+# while camera is turned on
+while(True):
+	#frame by frame capture from feed
+	ret, frame = cap.read()
+	
+	# convert fram to grey
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	# display image 
+	img = cv.imshow('frame',gray)
+
+	# publish 
+	# for (x,y,w,h) in faces:
+	# 	crop_faces = gray[y:y+h, x:x+w] # changed from img to gray FYI to follow pnpn
+	# 	cv.imshow("crop", crop_faces)
+	# 	client.publish("face_app/test", bytearray(cv.imencode('.png', crop_faces)[1]), qos=1)
+	
+	# q to quit 
+	if cv.waitKey(1) & 0xFF == ord('q'):
+		break
 
 # gray here is the gray frame you will be getting from a camera
 ##gray = cv.cvtColor(gray, cv.COLOR_BGR2GRAY)
@@ -12,7 +43,7 @@ face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 ##for (x,y,w,h) in faces:
 	# your logic goes here; for instance
 	# cut out face from the frame.. 
-	# rc,png = cv2.imencode('.png', face)
+	# rc,png = cv.imencode('.png', face)
 	# msg = png.tobytes()
 	# ...
 	#testing
