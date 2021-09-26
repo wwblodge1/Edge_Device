2826a0e9d3e868f22d88eec24049af49fd477b88
wwblodge1 <wwblodge@ischool.berkeley.edu>
2021-09-25 19:18:26 -0700

    Fixed local network issues

--- edge/cam/edge_docker.sh
+++ edge/cam/edge_docker.sh
@@ -1,3 +1,3 @@
-docker build -t cam --no-cache -f Dockerfile.ubuntu_NX .
+docker build -t cam -f Dockerfile.ubuntu_NX .
 docker tag cam wwblodge1/cam:v1
 docker push wwblodge1/cam:v1
--- edge/cam/face_video.py
+++ edge/cam/face_video.py
@@ -1,16 +1,11 @@
-
-
-import cv2 as cv
 import numpy as np
-import time
+import cv2
 import paho.mqtt.client as mqtt
-import sys
 
 LOCAL_MQTT_HOST="mosquitto-service"
 LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC="faces"
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect_local(client, userdata, flags, rc): 
     print("connected to local broker with rc: " + str(rc))
  
@@ -19,46 +14,32 @@ local_mqttclient.loop_start()
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
 local_mqttclient.on_connect = on_connect_local
 
-time.sleep(1) 
+cam = cv2.VideoCapture(0, cv2.CAP_V4L2)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# video feed capture 
-cap = cv.VideoCapture(0)
-
-# Cascade face classifier
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# while camera is turned on
 while(True):
-	#frame by frame capture from feed
-	ret, frame = cap.read()
-	
-	# convert fram to grey
-	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-	faces = face_cascade.detectMultiScale(gray, 1.3, 5) #ppz
+    # Capture frame-by-frame
+    ret, frame = cam.read()
 
-	# display image 
-	img = cv.imshow('frame',gray)
-	img
-	# publish 
-	for (x,y,w,h) in faces:
-		crop_faces = gray[y:y+h, x:x+w] # changed from img to gray FYI to follow pnpn
-		cv.imshow("crop", crop_faces)
-		local_mqttclient.publish(LOCAL_MQTT_TOPIC, bytearray(cv.imencode('.png', crop_faces)[1]), qos=1)
-	
-	#OR from instructions
-	# your logic goes here; for instance
-	# cut out face from the frame.. 
-	# rc,png = cv.imencode('.png', face)
-	# msg = png.tobytes() 
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        face_crop = gray[y:y+h, x:x+w]
+        rc,png = cv2.imencode('.png', face_crop)
+        msg = png.tobytes()
+        print(msg)
+        try:
+            local_mqttclient.publish(LOCAL_MQTT_TOPIC, msg, qos=0, retain=False)
+        except Exception as e:
+            print(e)
+       # frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+       # cv2.imshow('feed', frame)
+    # quit
+   # if cv2.waitKey(1) & 0xFF == ord('q'):
+       # cv2.destroyAllWindows()
+       # break
 
-	# q to quit 
-	if cv.waitKey(1) & 0xFF == ord('q'):
-		break
-
-time.sleep(1)
-
-# Fin
-client.loop_stop()
-client.disconnect()
+# When everything done, release the capture
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
