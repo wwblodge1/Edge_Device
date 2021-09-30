b9358fcbf12ec11a06521a7702b1146b14e7545e
wwblodge1 <wwblodge@ischool.berkeley.edu>
2021-09-29 23:47:43 -0700

    update back to normal

--- edge/msg_forwarder/forwarder.py
+++ edge/msg_forwarder/forwarder.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 import sys
 
+# https://www.eclipse.org/paho/index.php?page=clients/python/docs/index.php
+
 LOCAL_MQTT_HOST="mosquitto-service"
 LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC="faces"
@@ -9,25 +11,26 @@ REMOTE_MQTT_HOST="52.90.236.138"
 REMOTE_MQTT_PORT=32364
 REMOTE_MQTT_TOPIC="faces"
 
-def on_connect(client, userdata, flags, rc):
+def on_connect_local(client, userdata, flags, rc):
     print("connected to local broker with rc: " + str(rc))
     client.subscribe(LOCAL_MQTT_TOPIC)
 
-# The callback for when a PUBLISH message is received from the server.
+
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    try:
-        print("{} bytes recieved".format(len(msg.payload)))
-        msg = msg.payload
-        remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
+  print("into on_message to be publish")
+  try:
+    print("message received! {} bytes ".format(len(msg.payload)))
+    # if we wanted to re-publish this message, something like this should work
+    msg = msg.payload
+    remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+  except:
+    print("Unexpected error:", sys.exc_info()[0])
 
 print("Creating local instance")
 local_mqttclient = mqtt.Client()
 
 print("Bind call back function")
-local_mqttclient.on_connect = on_connect
+local_mqttclient.on_connect = on_connect_local
 
 print("Connect to local broker")
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
@@ -37,7 +40,6 @@ remote_mqttclient = mqtt.Client()
 
 print("Connect to remote broker")
 remote_mqttclient.connect(REMOTE_MQTT_HOST, REMOTE_MQTT_PORT, 60)
-print("It has connected!")
 
 print("Publishing message...")
 local_mqttclient.on_message = on_message
