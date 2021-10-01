dd5c1dbee2f87bfe4434402687d99716eef08d7b
Ubuntu <ubuntu@ip-172-31-16-216.ec2.internal>
2021-10-01 03:36:37 +0000

    jayanth

--- cloud/deploy_script.sh
+++ cloud/deploy_script.sh
@@ -4,7 +4,7 @@ if [ "$1"  == run ]; then
    kubectl apply -f ~/w251/cloud/broker/mqtt_deployment.yaml
    kubectl apply -f ~/w251/cloud/broker/mqtt_service.yaml
    kubectl apply -f ~/w251/cloud/processor/processor.yaml
-   kubectl expose deployment mosquitto-deployment --type=NodePort --port=32364 --name=mosquitto
+  # kubectl expose deployment mosquitto-deployment --type=NodePort --port=32364 --name=mosquitto
 else
    kubectl delete deployment mosquitto-deployment
    kubectl delete deployment processor-deployment 
